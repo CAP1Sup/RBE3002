8a90c2593501c70bf474c85603e135dd2a7d97ed
Christian Piper <xmanpiper@gmail.com>
2024-04-10 14:49:30 -0400

    Updated go_to_pose to check if position was reached, changed move_to_point to a service

--- lab3/src/move_to_point.py
+++ lab3/src/move_to_point.py
@@ -2,16 +2,18 @@
 
 import rospy
 import math
+import argparse
 from lab2.srv import GoToPoseStamped
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Bool
 from nav_msgs.msg import Odometry
 from nav_msgs.srv import GetPlan
 from geometry_msgs.msg import PoseStamped
+from lab2.srv import GoToPoseStamped
 
 
 class MoveToPoint:
 
-    def __init__(self):
+    def __init__(self, is_service=False):
         """
         Class constructor
         """
@@ -22,9 +24,13 @@ class MoveToPoint:
         # When a message is received, call self.update_odometry
         rospy.Subscriber("/odom", Odometry, self.update_odometry)
 
-        # Tell ROS that this node subscribes to PoseStamped messages on the '/move_base_simple/goal' topic
-        # When a message is received, call self.go_to
-        rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.move_to_point)
+        if is_service:
+            # Create a service called '/move_to_point' with the 'GoToPoseStamped' service type
+            rospy.Service("/move_to_point", GoToPoseStamped, self.move_to_point_service)
+        else:
+            # Tell ROS that this node subscribes to PoseStamped messages on the '/move_base_simple/goal' topic
+            # When a message is received, call self.go_to
+            rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.move_to_point)
 
         # Attributes to keep track of current position
         self.curr_pose = PoseStamped()
@@ -37,10 +43,26 @@ class MoveToPoint:
         """
         self.curr_pose = PoseStamped(pose=msg.pose.pose)
 
-    def move_to_point(self, goal: PoseStamped):
+    def move_to_point_service(self, req: GoToPoseStamped):
+        """
+        Moves the robot to a given point.
+        :param req [GoToPoseStamped] The request containing the goal and the speeds.
+        :return [Bool] True if the robot reached the goal.
+        """
+        result = self.move_to_point(req.goal, req.linear_speed, req.angular_speed)
+        return Bool(data=result)
+
+    def move_to_point(
+        self,
+        goal: PoseStamped,
+        linear_speed=Float32(data=0.2),
+        angular_speed=Float32(data=0.5),
+    ):
         """
         Moves the robot to a given point.
         :param goal [PoseStamped] The goal.
+        :param linear_speed [Float32] The linear speed.
+        :param angular_speed [Float32] The angular speed.
         """
         # Print the current position
         rospy.loginfo(
@@ -80,13 +102,28 @@ class MoveToPoint:
                     f"Moving to {pose.pose.position.x}, {pose.pose.position.y}"
                 )
                 # Move the robot to the next point
-                go_to_pose(pose, Float32(data=0.2), Float32(data=0.5))
+                reached = go_to_pose(pose, linear_speed, angular_speed)
+
+                # If the robot didn't reach the goal, return False
+                if not reached:
+                    return False
+
+            # Path was followed successfully, return True
+            return True
         except rospy.ServiceException as e:
             rospy.logerr(f"Service call failed: {e}")
+            return False
 
     def run(self):
         rospy.spin()
 
 
 if __name__ == "__main__":
-    MoveToPoint().run()
+    parser = argparse.ArgumentParser(description="Move to Point")
+    parser.add_argument(
+        "-s",
+        "--service",
+        action="store_true",
+    )
+    args, unknown = parser.parse_known_args()
+    MoveToPoint(args.service).run()
